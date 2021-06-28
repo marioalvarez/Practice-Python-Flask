23f53c51752f6fb6adf863f8581d9f0568a58a53
mario alvarez <mealvarez@economia.cl>
2021-06-27 23:52:35 -0400

    creando rutas

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ def index():
     return "Aprendiendo Flask con Mario Alvarez"
 
 
+@app.route('/informacion')
+def informacion():
+    return "<h1>Pagina de informacion</h1>"
+
+
+@app.route('/contacto')
+def contacto():
+    return "<h1>Pagina de contacto</h1>"
+
+
+@app.route('/lenguajes-de-programacion')
+def lenguajes():
+    return "<h1>Pagina de Lenguajes</h1>"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
